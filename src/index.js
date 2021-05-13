323cd2a2786d13ae4267669c04ae2aeb87aaa0bd
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-05-13 12:21:40 +0200

    Travail côté serveur et côté client
    Côté serveur
    --------------
    - Génération d'un token cryptographiquement
    sécurisé
    - Remplacement de Math.random par le module
    crypto pour obtenir un aléatoire non prédictible
    - Création d'un fichier data.json s'il n'existe pas.
    Ce fichier est là à but temporaire pour stocker
    les associations token - id à guess.
    
    Côté client
    -------------
    - Ajout d'un champ caché contenant la valeur
    du bouton sur lequel on a cliqué.
    Le contenu du formulaire n'est pas
    automatiquement transmis dès le clic sur une
    image, cela en vue de l'étape 2.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const assert = require('assert')
+const crypto = require('crypto')
 const fs = require('fs')
 const path = require('path')
 
@@ -13,17 +14,18 @@ const SINGULAR_DIR = path.join(BASE_DIR, 'singuliers')
 // IN-MEM JSON
 const HINTS_JSON = JSON.parse(fs.readFileSync(path.join(BASE_DIR, 'indices.json')))
 Object.freeze(HINTS_JSON)
+const DATA_JSON = JSON.parse(readOrCreateJson('data.json', '{}'))
 
-fastify.get('/', async (request, reply) => {
-    reply.type('text/html').send(fs.readFileSync('index.html', {encoding: 'utf-8'}))
-})
-
-function randInRange(min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
+function readOrCreateJson(filePath, defaultContent) {
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, defaultContent)
+        return defaultContent
+    }
+    return fs.readFileSync(filePath)
 }
 
 function pickRandomImage(files) {
-    return files[randInRange(0, files.length)]
+    return files[crypto.randomInt(0, files.length)]
 }
 
 function pick7NeutralImages() {
@@ -49,7 +51,7 @@ function base64image(filePath) {
 }
 
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5)
+    array.sort(() => crypto.randomInt(100))
 }
 
 function findImageIndex(arr, imgPath) {
@@ -68,10 +70,22 @@ function addImagesToSet(set, basedir, imgArr) {
     }
 }
 
+function writeData(token, idToGuess) {
+    DATA_JSON[token] = idToGuess
+    fs.writeFileSync('data.json', JSON.stringify(DATA_JSON))
+}
+
+function generateToken() {
+    return crypto.randomBytes(20).toString('hex');
+}
+
+fastify.get('/', async (request, reply) => {
+    reply.type('text/html').send(fs.readFileSync('index.html', {encoding: 'utf-8'}))
+})
+
 fastify.get('/api/newsession', async (request, reply) => {
     const neutral = Array.from(pick7NeutralImages())
     const singular = pickRandomImage(fs.readdirSync(SINGULAR_DIR))
-    // TODO Générer token, image data en base64 et conserver id de l'image singulière
 
     const set = new Set()
     addImagesToSet(set, NEUTRAL_DIR, neutral)
@@ -80,16 +94,18 @@ fastify.get('/api/newsession', async (request, reply) => {
     const arr = Array.from(set)
     shuffle(arr)
 
-    // TODO To use
     const singularIndex = findImageIndex(arr, singular)
+    const token = generateToken()
+
+    writeData(token, singularIndex)
 
     // On enlève le nom de l'array
     const images = arr.map(item => item['data'])
 
-    // TODO Trouver singular dans l'array et récupérer son id pour le conserver
     reply.type("text/json").send({
         hint: HINTS_JSON[singular],
-        images: images
+        images: images,
+        token: token
     })
 })
 
