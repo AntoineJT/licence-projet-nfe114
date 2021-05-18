59e7cefdd7f9b6d41ff72fd8fddee439d620b9cc
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-05-18 11:13:58 +0200

    Tentatives de reload

--- src/public/captchat.js
+++ src/public/captchat.js
@@ -9,30 +9,44 @@
         return
     }
 
-    const jsonText = await req("/api/newsession")
-    const json = JSON.parse(jsonText)
-    // console.log(json)
+    (async function reload() {
+        const jsonText = await req("/api/newsession")
+        const json = JSON.parse(jsonText)
+        // console.log(json)
 
-    captchat.innerHTML += `<h2>Indice : ${json['hint']}</h2>`
-    captchat.innerHTML += "<div>"
-    // temp scope to destroy index variable
-    {
-        let index = 0
-        for (const item of json.images) {
-            captchat.innerHTML += `<button class="captchat-btn" value="${index}"><img src="${item}"></button>`
-            ++index
+        captchat.innerHTML += `<h2>Indice : ${json['hint']}</h2>`
+        captchat.innerHTML += "<div>"
+        // temp scope to destroy index variable
+        {
+            let index = 0
+            for (const item of json.images) {
+                captchat.innerHTML += `<button class="captchat-btn" value="${index}"><img src="${item}"></button>`
+                ++index
+            }
         }
-    }
-    captchat.innerHTML += `<input type="hidden" name="captchat-token" value="${json.token}">`
-    captchat.innerHTML += "</div>"
+        captchat.innerHTML += `<input type="hidden" name="captchat-token" value="${json.token}">`
+        captchat.innerHTML += '<input type="hidden" disabled name="reload" value="0">'
+        captchat.innerHTML += "</div>"
 
-    // add selected image to the field
-    const buttons = document.querySelectorAll('.captchat-btn')
-    buttons.forEach(btn => {
-        btn.addEventListener('click', async () => {
-            const selected = btn.getAttribute('value')
-            const res = await req(`/api/validate?token=${json.token}&guess=${selected}`)
-            alert(JSON.parse(res).success)
+        // add selected image to the field
+        const buttons = document.querySelectorAll('.captchat-btn')
+        buttons.forEach(btn => {
+            btn.addEventListener('click', async () => {
+                const selected = btn.getAttribute('value')
+                const res = await req(`/api/validate?token=${json.token}&guess=${selected}`)
+                alert(JSON.parse(res).success)
+            })
+        })
+    })().then(() => {
+        const reloadTrigger = document.querySelector('input[name=reload]')
+        reloadTrigger.addEventListener('change', async () => {
+            const val = reloadTrigger.getAttribute('value')
+            console.log(val)
+            if (val != 1)
+                return
+
+            reloadTrigger.setAttribute('value', 0)
+            await reload()
         })
     })
 })()
