279fb267a1039cfc8d2d97d3e0167a3d41622474
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-05-18 10:39:41 +0200

    Tests infructueux

--- src/public/captchat.js
+++ src/public/captchat.js
@@ -1,4 +1,4 @@
-(async () => {
+async function captchat_reload() {
     async function req(url) {
         return await fetch(url).then(data => data.text())
     }
@@ -23,6 +23,10 @@
             ++index
         }
     }
+
+    captchat.innerHTML += '<canvas id="horloge" width="300" height="300"></canvas>'
+    captchat.innerHTML += '<div id="temps"></div>'
+
     captchat.innerHTML += `<input type="hidden" name="captchat-token" value="${json.token}">`
     captchat.innerHTML += "</div>"
 
@@ -32,7 +36,12 @@
         btn.addEventListener('click', async () => {
             const selected = btn.getAttribute('value')
             const res = await req(`/api/validate?token=${json.token}&guess=${selected}`)
+            // TODO faire un timer
+            // TODO Utiliser la réponse pour réduire timer
+            // TODO Reload
             alert(JSON.parse(res).success)
         })
     })
-})()
+}
+
+(async() => await captchat_reload())()
