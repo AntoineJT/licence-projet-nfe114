21d310b12aa9e37c30dc76ac87850f42dc64956d
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-05-18 19:46:01 +0200

    everything is fucking broken, save me!

--- src/public/captchat.js
+++ src/public/captchat.js
@@ -9,15 +9,19 @@ async function captchat_reload () {
         return
     }
 
-    captchat.innerHTML = ''
+    const captmain = document.getElementById("captchat-main")
+
+    if (captmain !== null)
+        captmain.innerHTML = ''
 
     const jsonText = await req("/api/newsession")
     const json = JSON.parse(jsonText)
     // console.log(json)
 
     {
-        let buffer = `<h2>Indice : ${json['hint']}</h2>`
-        buffer += "<div>"
+        let buffer = '<div id="captchat-main">'
+        buffer += `<h2>Indice : ${json['hint']}</h2>`
+        buffer += '<div>'
         // temp scope to destroy index variable
         {
             let index = 0
@@ -27,9 +31,18 @@ async function captchat_reload () {
             }
         }
         buffer += `<input type="hidden" name="captchat-token" value="${json.token}">`
-        buffer += "</div>"
+        buffer += "</div></div>"
 
-        captchat.innerHTML += buffer
+        // if first run
+        if (captmain === null) {
+            buffer += `<div id="bloc-horloge">
+                <canvas id="horloge" width="300" height="300"></canvas>
+                <div id="temps"></div>
+            </div>`
+            captchat.innerHTML = buffer
+        } else {
+            captmain.innerHTML += buffer
+        }
     }
 
     // add selected image to the field
@@ -50,15 +63,99 @@ async function captchat_reload () {
         })
     })
 }
-captchat_reload()
+
+let temps
+
+captchat_reload().then(_ => {
+    temps = document.getElementById('temps')
+
+    runHorloge()
+})
 
 // réduit le temps de 5s
 function captchat_reload_err() {
     t = Math.max(refreshTime, t - 5000)
     captchat_reload()
+    runHorloge()
 }
 
 const refreshTime = 10000
 let t = refreshTime + 20000
 
 const refresh = window.setTimeout(() => window.setInterval(() => captchat_reload_err(), refreshTime), t - refreshTime)
+
+// HORLOGE
+
+function runHorloge() {
+    const debut = new Date()
+    debuter(debut.getTime(), t)
+}
+
+function debuter(debut, timer) {
+    const d = new Date()
+    window.intOffset = timer - (d.getTime() - debut)
+
+    temps.innerHTML = Math.ceil(window.intOffset / 1000)
+
+    const mult = 60000 / t
+    window.angle = 0.1048335 * 0.001 * mult * window.intOffset
+
+    if (window.intOffset <= 0) {
+        return
+    }
+    drawHorloge(1)
+
+    window.t = setTimeout(`debuter(${debut}, ${timer})`, 16)
+}
+
+function changeColor(angle) {
+    angle = 6.29 - angle;
+    const r = Math.floor(72 + 55 * angle)
+    const b = Math.floor(214 + 14 * angle)
+
+    return (r < 255 || b < 255)
+        ? `rgb(${r}, ${b}, 0)`
+        : `rgb(${Math.floor(255)}, ${Math.floor(597 - (90 * angle))}, 0)`
+}
+
+function drawHorloge(pourcent) {
+    const canvas = document.getElementById("horloge")
+    const ctx = canvas.getContext("2d")
+    const twoPI = Math.PI * 2
+
+    ctx.clearRect(0, 0, 300, 300)
+
+    /*
+    ctx.beginPath()
+    ctx.globalAlpha = 1
+    ctx.arc(150, 150, 140, 0, 6.283, false)
+    ctx.arc(150, 150, 105, 6.283, twoPI, true)
+    ctx.fillStyle = "#bbb"
+    ctx.fill()
+    ctx.closePath()
+    */
+
+    ctx.beginPath()
+    ctx.globalAlpha = 1
+    // ctx.arc(150, 150, 140.1, -1.57, (-1.57 + window.angle), false)
+    // ctx.arc(150, 150, 105, (-1.57 + window.angle), (twoPI - 1.57), true)
+    ctx.fillStyle = changeColor(window.angle)
+
+    // new
+    ctx.fillRect(10, 50, 65, 400)
+    ctx.fillStyle = "#bbb"
+    ctx.fillRect(10, 50, 65, 400 * (window.pourcent / 100))
+    // end new
+
+    ctx.fill()
+    ctx.closePath()
+
+    /*
+    ctx.beginPath()
+    ctx.arc(150, 150, (105 * pourcent), 0, 6.283, false)
+    ctx.fillStyle = "#fff"
+    ctx.fill()
+    ctx.closePath()
+    */
+}
+// END HORLOGE
